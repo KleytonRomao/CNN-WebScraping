5ae081b1854f55e0ce1d90487f3e23398a6398c3
Kleyton Romão <86174204+KleytonRomao@users.noreply.github.com>
2025-10-11 05:17:52 +0000

    0.3

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from utils.scraper import *
 
 if __name__ == "__main__":
     url = "https://www.cnnbrasil.com.br/internacional/"
-    data = scrape_website(url)
-    print(data)
+    data = Scraper(url)
+    txt = data.scrape_website()
+    for i in txt:
+        print(i.get_text())
