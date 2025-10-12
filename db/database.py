f3162cab70724f76b4f7d35d94ad57490fa98886
Kleyton Romão <86174204+KleytonRomao@users.noreply.github.com>
2025-10-12 06:36:12 +0000

    0.4

--- db/database.py
+++ db/database.py
@@ -2,6 +2,11 @@ from peewee import *
 
 db = SqliteDatabase('database.db')
 
+def create(db, noticias):
+    db.connect()
+    db.create_tables([noticias], safe=True)
+    db.close()
+
 class noticias(Model):
     conteudo = TextField()
     data = DateTimeField(constraints=[SQL('DEFAULT CURRENT_TIMESTAMP')])
@@ -9,5 +14,4 @@ class noticias(Model):
     class Meta:
         database = db
 
-db.connect()
-db.create_tables([noticias])
+create(db, noticias)
